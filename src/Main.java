5ae38c994c22ebcb8373965fa707bbdae61c3bcc
AndrewSmooth <gladkiy.a2004@gmail.com>
2023-09-14 08:46:12 +0300

    Hello world1111

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World111111!");
         //1
         System.out.println("---Task 1---");
         System.out.println(convert(5));
